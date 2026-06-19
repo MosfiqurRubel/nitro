742f647283fa6c797f0a291019fd9ffc39c37b3f
MosfiqurRubel <rubel.airin@gmail.com>
2026-06-19 17:56:31 +0600

    ready to build

--- src/components/sections/ProductionSection.tsx
+++ src/components/sections/ProductionSection.tsx
@@ -2,19 +2,10 @@ import FootballImage from "@/assets/images/football-player.png";
 
 export default function ProductionSection() {
   return (
-    <section
-      id="production-section"
-      className="production-section"
-    >
-      {/* Background blur */}
-      <div
-        className="bg-blur-box"
-      />
+    <section id="production-section" className="production-section">
+      <div className="bg-blur-box" />
 
-      {/* Football image */}
-      <div
-        className="football-area"
-      >
+      <div className="football-area">
         <img
           src={FootballImage}
           alt="Football player"
@@ -22,16 +13,9 @@ export default function ProductionSection() {
         />
       </div>
 
-      {/* GSAP target */}
-      <div
-        id="production-target"
-        className="production-target"
-      />
+      <div id="production-target" className="production-target" />
 
-      {/* Title */}
-      <div
-        className="relative z-20 mt-12 flex justify-center lg:absolute lg:right-20 lg:top-1/2 lg:mt-0 lg:-translate-y-1/2"
-      >
+      <div className="relative z-99991 mt-12 flex justify-center lg:absolute lg:right-20 lg:top-1/2 lg:mt-0 lg:-translate-y-1/2">
         <h2 className="production-title">SINCE 2002 PRODUCING</h2>
       </div>
     </section>
